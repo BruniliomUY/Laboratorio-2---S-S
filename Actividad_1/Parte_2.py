6572bede5adfef4bcb4d9fe377ca1e00d463a355
Bruno <brunomoreira.uy@gmail.com>
2025-10-31 12:03:17 -0300

    Commit_3

--- Actividad_1/Parte_2.py
+++ Actividad_1/Parte_2.py
@@ -9,7 +9,7 @@ def senoidal(freq, sample_rate, duration):
     y = np.cos((2 * np.pi) * frequencies)
     return x, y
      
-def CTFT(x, fs, title="Espectro de Magnitud (FFT)"):
+def DFT(x, fs, title="Espectro de Magnitud (FFT)"):
     """
     Calcula y grafica la FFT de una señal x muestreada a frecuencia fs.
 
@@ -23,8 +23,6 @@ def CTFT(x, fs, title="Espectro de Magnitud (FFT)"):
     X_shifted = fftshift(X)
     freqs = fftshift(fftfreq(N, d=1.0 / fs))
     X_mag = np.abs(X_shifted)
-
-
     # Gráfico
     plt.figure(figsize=(8,4))
     plt.plot(freqs, X_mag)
@@ -35,6 +33,39 @@ def CTFT(x, fs, title="Espectro de Magnitud (FFT)"):
     plt.show()
 
     return freqs, X_mag
+
+def CTFT(x, fs, title="Espectro de Magnitud (FFT con Zero-Padding)", padding_factor=4):
+    """
+    Calcula y grafica la FFT de una señal x muestreada a frecuencia fs con zero-padding.
+
+    Parámetros:
+        x  : array, señal en el tiempo
+        fs : float, frecuencia de muestreo [Hz]
+        title : str, título del gráfico
+        padding_factor : int, factor de aumento del tamaño (default=4)
+    """
+    N = len(x)
+    N_pad = padding_factor * N   # nuevo tamaño (4 veces más puntos)
+    
+    # FFT con zero-padding
+    X = fft(x, N_pad)
+    X_shifted = fftshift(X)
+    freqs = fftshift(fftfreq(N_pad, d=1.0 / fs))
+    X_mag = np.abs(X_shifted)
+
+    # Gráfico
+    plt.figure(figsize=(8,4))
+    plt.plot(freqs, X_mag)
+    plt.title(title)
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("|X(f)|")
+    plt.grid(True)
+    plt.show()
+
+    return freqs, X_mag
+
+
+
  
 #Ejemplo fs<fmax no cumple la condicion de Nyquist, se produce aliasing
 
@@ -47,8 +78,8 @@ plt.plot(x, y)
 plt.ylim(1, -1)
 plt.show()
 
+DFT(y, SAMPLE_RATE)
 CTFT(y, SAMPLE_RATE)
-
 
 #Ejemplo fs>fmax cumple la condicion de Nyquist, se produce aliasing
 
@@ -61,4 +92,5 @@ plt.plot(x, y)
 plt.ylim(1, -1)
 plt.show()
 
+DFT(y, SAMPLE_RATE)
 CTFT(y, SAMPLE_RATE)
